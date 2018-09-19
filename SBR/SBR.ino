c118a462dd5bf40869bf22975c22176c163f233b
tferguson3 <43049991+tferguson3@users.noreply.github.com>
2018-09-19 13:43:49 -0800

    TFF Edits 9.19

--- SBR/SBR.ino
+++ SBR/SBR.ino
@@ -455,23 +455,24 @@ void loop() {
    //aeration(60,17);
    //settle(50, 19);//delay(1000);
    //decant(6);//loading();
+    normalsequence();
     x++;}
   
     //this section checks what day it is then starts the sequence if time of day is between these hours
   if (now.dayOfTheWeek()== 1){//"Monday"
-    if (now.hour()>4 && now.hour()<6){normalsequence();}}
+    if (now.hour()>5 && now.hour()<7){normalsequence();}}
   if (now.dayOfTheWeek()== 2){//"Tuesday"
-    if (now.hour()>4 && now.hour()<6){normalsequence();}}
+    if (now.hour()>5 && now.hour()<7){normalsequence();}}
   if (now.dayOfTheWeek()== 3){//"Wednesday"
-    if (now.hour()>4 && now.hour()<6){normalsequence();}}
+    if (now.hour()>5 && now.hour()<7){normalsequence();}}
   if (now.dayOfTheWeek()== 4){//"thursday"
-    if (now.hour()>4 && now.hour()<6){normalsequence();}}
+    if (now.hour()>5 && now.hour()<7){normalsequence();}}
   if (now.dayOfTheWeek()== 5){//"Friday"
-    if (now.hour()>4 && now.hour()<6){normalsequence();}}
+    if (now.hour()>5 && now.hour()<7){normalsequence();}}
   if (now.dayOfTheWeek()== 6){//"Saturday"
-    if (now.hour()>4 && now.hour()<6){normalsequence();}}//fillplusair(21,1);thirtysecaeration(100,10);}}
+    if (now.hour()>5 && now.hour()<7){normalsequence();}}//fillplusair(21,1);thirtysecaeration(100,10);}}
   if (now.dayOfTheWeek()== 0){//"Sunday"
-    if (now.hour()>4 && now.hour()<6){normalsequence();}}//weekend();}
+    if (now.hour()>5 && now.hour()<7){normalsequence();}}//weekend();}
   delay(20000);
   ////This weekend function is poorly named but means what happens before the sequence is triggered and after it is done
   weekend();}
